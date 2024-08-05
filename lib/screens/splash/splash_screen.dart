dbcda4eb364885c0fdad50e1c0b7949fb3ffdcf6
vishnudev1629 <vishnucherupuzha2020@gmail.com>
2024-08-05 14:35:33 +0530

    File moved to folder in auth

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:ninetytwoagents/screens/login_screen.dart';
+import 'package:ninetytwoagents/screens/auth/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
